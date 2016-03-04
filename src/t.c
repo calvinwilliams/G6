c3b2ad7a795f48522382f5890b45a24555fcacce
hotmocha <hotmocha@126.com>
2016-03-04 09:15:39 +0800

    ...

---
+++ src/t.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <unistd.h>
+
+int g_exit_flag = 0 ;
+
+static void sig_proc( int sig_no )
+{
+	if( sig_no == SIGUSR2 )
+	{
+		pid_t	pid ;
+		
+		pid = fork() ;
+		if( pid == -1 )
+		{
+			exit(9);
+		}
+		else if( pid == 0 )
+		{
+			execlp( "t" , "t" , NULL );
+			exit(9);
+		}
+		
+		g_exit_flag = 1 ;
+	}
+	
+	return;
+}
+
+int main()
+{
+	signal( SIGUSR2 , & sig_proc );
+	
+	while( g_exit_flag == 0 )
+	{
+		sleep(1);
+	}
+	
+	return 0;
+}
+
